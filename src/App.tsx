b9fe839e1225f4af08d477e653d70bbc07359003
SahilRajpal-hub <sahilrajpal.31586@gmail.com>
2021-12-30 17:54:17 +0530

    environment variables set and proxy removed

--- src/App.tsx
+++ src/App.tsx
@@ -86,6 +86,7 @@ const App = () => {
             const { data } = await axios.get(`${process.env.REACT_APP_SECRET_URL}/api/v1/snippet/`);
             console.log(data);
             setFiles(data);
+
         }
 
         fetchData();
